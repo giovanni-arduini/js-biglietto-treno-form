6b956f37eeeff0e4efe9dacb5b67515b9d5419ab
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-23 15:15:34 +0200

    ADD logic to form

--- app.js
+++ app.js
@@ -14,33 +14,33 @@ formElement.addEventListener("submit", (event) => {
   console.log("ho inviato il modulo");
 
   const travelLength = travelLengthElement.value;
-  console.log(travelLength);
+  //   console.log(travelLength);
   const userAge = userAgeElement.value;
-  console.log(userAge);
+  //   console.log(userAge);
   const standardPrice = travelLength * 0.21;
-  console.log(standardPrice);
+  //   console.log(standardPrice);
+  if (
+    !isNaN(travelLength) &&
+    !isNaN(userAge) &&
+    travelLength >= 0 &&
+    userAge >= 0
+  ) {
+    function calculateFinalPrice(userAge) {
+      return userAge < 18
+        ? `Il prezzo scontato per minorenni è di ${(
+            standardPrice -
+            standardPrice * discountJunior
+          ).toFixed(2)}`
+        : userAge >= 65
+        ? `Il prezzo scontato per Over65 è di ${(
+            standardPrice -
+            standardPrice * discountSenior
+          ).toFixed(2)}`
+        : `Il prezzo del tuo viaggio è di ${standardPrice.toFixed(2)}€`;
+    }
+    console.log(calculateFinalPrice(userAge));
+    console.log(userAge);
+  } else {
+    alert("I dati inseriti non sono validi!");
+  }
 });
-
-// if (
-//   !isNaN(travelLength) &&
-//   !isNaN(userAge) &&
-//   travelLength >= 0 &&
-//   userAge >= 0
-// ) {
-//   function calculateFinalPrice(userAge) {
-//     return userAge < 18
-//       ? `Il prezzo scontato per minorenni è di ${(
-//           standardPrice -
-//           standardPrice * discountJunior
-//         ).toFixed(2)}`
-//       : userAge >= 65
-//       ? `Il prezzo scontato per Over65 è di ${(
-//           standardPrice -
-//           standardPrice * discountSenior
-//         ).toFixed(2)}`
-//       : `Il prezzo del tuo viaggio è di ${standardPrice.toFixed(2)}€`;
-//   }
-//   console.log(calculateFinalPrice(userAge));
-// } else {
-//   //   alert("I dati inseriti non sono validi!");
-// }
